4dd89d01d80452c6b2990b56ba405af41bb4c844
nusrat-git <nusratjahanmithila66@gmail.com>
2022-11-08 23:04:19 +0600

    data loeaded from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,23 +8,59 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+require('dotenv').config();
+
 
 app.get('/', (req, res) => {
   res.send('Pass comments server running')
 })
 
 
+const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Password}@cluster0.rydkcco.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+  try {
+
+    const dataBase = client.db('passComments').collection('services');
+
+    app.get('/home', async (req, res) => {
+
+      const query = {};
+      const cursor = dataBase.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+
+    })
+
+    app.get('/services', async (req, res) => {
+
+      const query = {};
+      const cursor = dataBase.find(query);
+      const AllServices = await cursor.toArray();
+      res.send(AllServices);
+
+    })
+
+    app.get('/details/:id', async (req, res) => {
+
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const details = await dataBase.findOne(query);
+      res.send(details);
+
+    })
+
+  }
+  finally {
 
 
-// const uri = "mongodb+srv://<username>:<password>@cluster0.rydkcco.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+  }
 
+}
 
+run().catch(err => console.log(err));
 
 
 app.listen(port, () => {
